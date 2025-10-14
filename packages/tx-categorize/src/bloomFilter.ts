d3d3e1bbbdf9e53f2dbad7d84d31be05fe4e33ce
JP <jp.sainsbury@consensys.net>
2025-10-14 11:01:07 +0100

    feat: fix pl

--- packages/tx-categorize/src/bloomFilter.ts
+++ packages/tx-categorize/src/bloomFilter.ts
@@ -6,35 +6,38 @@ import { ChainId } from './networks'
 
 const chainIds = Object.values(ChainId).map(Number)
 
-const bloomByNetwork = chainIds.reduce((bloomByNetworkAcc, chainId) => {
-  const schemas = getSchemaByChainId(chainId as ChainId)
+const bloomByNetwork = chainIds.reduce(
+  (bloomByNetworkAcc, chainId) => {
+    const schemas = getSchemaByChainId(chainId as ChainId)
 
-  if (!schemas?.length) {
-    bloomByNetworkAcc[chainId] = undefined
+    if (!schemas?.length) {
+      bloomByNetworkAcc[chainId] = undefined
+
+      return bloomByNetworkAcc
+    }
+
+    const bloom = BloomFilter.create(schemas.length, 0.01)
+
+    schemas.forEach(({ schema }) => {
+      const keys = Object.keys(schema)
+
+      keys.forEach((key) => {
+        if (Array.isArray(schema[key])) {
+          schema[key].forEach((value) => {
+            bloom.add(`${key}#${value}`)
+          })
+        } else {
+          bloom.add(`${key}#${schema[key]}`)
+        }
+      })
+    })
+
+    bloomByNetworkAcc[chainId] = bloom
 
     return bloomByNetworkAcc
-  }
-
-  const bloom = BloomFilter.create(schemas.length, 0.01)
-
-  schemas.forEach(({ schema }) => {
-    const keys = Object.keys(schema)
-
-    keys.forEach((key) => {
-      if (Array.isArray(schema[key])) {
-        schema[key].forEach((value) => {
-          bloom.add(`${key}#${value}`)
-        })
-      } else {
-        bloom.add(`${key}#${schema[key]}`)
-      }
-    })
-  })
-
-  bloomByNetworkAcc[chainId] = bloom
-
-  return bloomByNetworkAcc
-}, {} as Record<ChainId, BloomFilter>)
+  },
+  {} as Record<ChainId, BloomFilter>,
+)
 
 export const doesTxMatchSchema = (chainId: ChainId, transaction: Transaction) => {
   const keysToCheck = ['toAddress', 'fromAddress', 'topics', 'logAddress', 'methodId']
